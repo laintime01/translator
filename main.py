a16c8a1f6734a8090f245c1aa25b710629158d67
laintime <hzhe111@163.com>
2022-08-26 22:36:53 +0800

    init commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# This is auto translate program by hao
+import server
+import client
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print('PyCharm')
+
